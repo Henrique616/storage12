d0c2ea7cea4deaa410297bf1ff1a4b5046bc9c43
Henrique616 <gustavohenriquemonte616@gmail.com>
2023-08-26 19:19:51 -0300

    ?
    ...

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.alunoView, name='alunos-lista')
+    path('', views.alunoView, name='alunos-lista'),
+    path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'),
+
 ]
